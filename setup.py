c71e7272b601f5c501e4e9310c497ed7553b489a
Kensuke-Mitsuzawa <kensuke.mit@gmail.com>
2019-10-27 19:29:54 +0100

    version 0.15

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import unittest
 import codecs
 
 name = 'flexible_clustering_tree'
-version = '0.14'
+version = '0.15'
 description='easy interface for ensemble clustering'
 author = 'Kensuke Mitsuzawa'
 author_email = 'kensuke.mit@gmail.com'
